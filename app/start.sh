8dcd561ecc3dcfa135448ea7fb8d340d2e035c37
jo-naka <atom6ur6s@gmail.com>
2022-08-21 23:49:46 +0900

    fix: Alpine(Docker)でVMにFAKETIMEが定義されていないとエラーなる問題に対応 #6

--- app/start.sh
+++ app/start.sh
@@ -1,17 +1,23 @@
+setdate="2020-01-01"
+settime="00:00:00"
 if [ "$(uname)" == 'Darwin' ]; then
 	export DYLD_FORCE_FLAT_NAMESPACE=1
 	export DYLD_INSERT_LIBRARIES=/opt/homebrew/lib/faketime/libfaketime.1.dylib
+else
+	# 初回にFAKETIMEが定義されていないとエラーになる為設定（_start_at_形式で登録)
+	export FAKETIME="@${setdate} ${settime}"
 fi
+# FAKETIME_NO_CACHEが0の場合，環境変数FAKETIME変更時の反映に少し時間がかかる為
 export FAKETIME_NO_CACHE=1
 
 # ダミーデータの生成
-# node generate_dummydata.js > dummydata.json
+node generate_dummydata.js > dummydata.json
 # ダミーデータを読み込む
-# node import_dummydata.js dummydata.json
+node import_dummydata.js dummydata.json
 
 # シミュレーションの実行
 # -- 過去のシミュレーション結果を削除し，2020年1月1日から365日間のシミュレーションを行う
-node simulate.js --init 2020-01-01 -d 3
+node simulate.js --init "${setdate}" -d 30
 
 # シミュレーションの実行
 # -- 過去のシミュレーションの続きとして1000日間のシミュレーションを行う
