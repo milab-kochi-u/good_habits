9467e954fcd7e5464e6126ea585add2a97ddd01a
jo-naka <atom6ur6s@gmail.com>
2023-11-27 12:23:37 +0900

    add: 推薦に必要なライブラリ導入など

--- pyApp/app/main.py
+++ pyApp/app/main.py
@@ -1,7 +1,8 @@
+from pydantic import BaseModel
 from fastapi import FastAPI, File, UploadFile
+from recommended_model import cf_mem_user
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -9,5 +10,13 @@ def read_root():
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-  return {"filename" : file.filename}
+async def create_upload_file(input_file: UploadFile):
+    return {
+        "filename" : input_file.filename,
+        "content_type" : input_file.content_type,
+    }
+
+@app.post("/exec_cf_mem_user/users/{user_id}/works/{work_id}")
+async def exec_cf_mem_user(input_file: UploadFile, user_id: int, work_id: int):
+    await cf_mem_user.main(input_file, user_id, work_id)
+    return {"ok"}
