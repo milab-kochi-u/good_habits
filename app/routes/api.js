3bf67dee12c134d1cd42e464be9c479a134d47a6
jo-naka <atom6ur6s@gmail.com>
2023-12-18 09:42:55 +0900

    update: ファイルアップロード機能の実装途中

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/index.js');
-const dayjs = require('dayjs');
 const { exec } = require('child_process');
+// 2023.12.18 Express.js ファイルアップロード (multer 編) https://qiita.com/tadnakam/items/d21d014e09cfa98437a8
+const fs = require('fs');
+const multer = require('multer');
+const updir = "/tmp";
+const upload = multer({dest:updir}).single('file');
+
 
 /* API routes */
 router.get('/simulate', async function(req, res, next){
@@ -12,6 +17,30 @@ router.get('/simulate', async function(req, res, next){
       res.status(409).send('[ERROR]既に起動されています．時間を空けて再度リクエストしてください．');
     }else{
       res.send(`stdout: ${stdout}`);
+    }
+  });
+});
+
+// 2023.12.18 Express.js ファイルアップロード (multer 編) https://qiita.com/tadnakam/items/d21d014e09cfa98437a8
+router.post('/upload_single', (req, res) => {
+  upload(req,res,function(err){
+    if(err instanceof multer.MulterError){
+      console.log(err);
+      res.status(415).send("multer ERR!");
+      return;
+    }else if(err){
+      console.log(err);
+      res.status(415).send("other ERR!");
+      return;
+    }
+    const path = req.file.path.replace(/\\/g, "/");
+    if (path) {
+      const dest = updir + "/" + req.file.originalname;
+      fs.renameSync(path, dest);  // 長い一時ファイル名を元のファイル名にリネームする。
+      res.status(200).send(`${dest} にアップロードされました。`);
+    }
+    else {
+      res.send("エラー：アップロードできませんでした。");
     }
   });
 });
